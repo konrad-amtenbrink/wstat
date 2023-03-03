dea1d796c76e2effa64d8186af17d932a22f9704
Konrad Amtenbrink <konrad02@gmx.de>
2023-03-03 11:20:04 +0100

    feat: add weather history check for static dates

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 use tide::Request;
-use reqwest::Error;
+use std::collections::HashMap;
 use serde::Deserialize;
+use serde_json::Value;
+use anyhow::Result;
 
+#[derive(Debug, Deserialize)]
+struct Weather {
+    latitude: String,
+    longitude: String
+}
+
+#[derive(Deserialize, Debug)]
+struct JSONResponse {
+    json: HashMap<String, String>,
+}
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
@@ -12,25 +24,30 @@ async fn main() -> tide::Result<()> {
 }
 
 async fn get_status(_req: Request<()>) -> tide::Result {
-    get_weather();
-    Ok(format!("Ok").into())
+    let resp: bool = get_weather()?;
+    Ok(format!("{resp}", resp = resp).into())
 }
 
 #[tokio::main]
-async fn get_weather() -> Result<(), Error> {
+async fn get_weather() -> Result<bool, anyhow::Error> {
     let request_url = format!("https://archive-api.open-meteo.com/v1/archive?{latitude}&{longitude}&{daily}&{start}&{end}&{timezone}",
                           latitude = "latitude=52.52",
                           longitude = "longitude=13.41",
                           daily = "daily=temperature_2m_min",
-                          start = "start_date=2023-01-27",
-                          end = "end_date=2023-02-26",
+                          start = "start_date=2023-01-01",
+                          end = "end_date=2023-01-08",
                           timezone = "timezone=GMT-0");
 
-    let response = reqwest::get(&request_url)
-        .await
-        .unwrap()
-        .text()
-        .await;
-    println!("{:?}", response);
-    Ok(())
+    let response = reqwest::get(&request_url).await?.json::<serde_json::Value>().await?;
+
+    let mut negative_temperature: bool = false;
+    for i in 0..7 {
+        let value: f64 = serde_json::from_value(response["daily"]["temperature_2m_min"][i].clone())?;
+        if value < 1.0 {
+            negative_temperature = true;
+            break;
+        }
+    }
+
+    Ok(negative_temperature)
 }
