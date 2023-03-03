e8eef6084fc5f936f6e093e6b05ee88696ff87e4
Konrad Amtenbrink <konrad02@gmx.de>
2023-03-03 09:30:54 +0100

    feat: add basic status endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
 use tide::Request;
-use tide::prelude::*;
+use reqwest::Error;
+use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
-struct Animal {
-    name: String,
-    legs: u16,
-}
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let mut app = tide::new();
-    app.at("/orders/shoes").post(order_shoes);
+    app.at("/status").get(get_status);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
 
-async fn order_shoes(mut req: Request<()>) -> tide::Result {
-    let Animal { name, legs } = req.body_json().await?;
-    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
+async fn get_status(_req: Request<()>) -> tide::Result {
+    get_weather();
+    Ok(format!("Ok").into())
+}
+
+#[tokio::main]
+async fn get_weather() -> Result<(), Error> {
+    let request_url = format!("https://archive-api.open-meteo.com/v1/archive?{latitude}&{longitude}&{daily}&{start}&{end}&{timezone}",
+                          latitude = "latitude=52.52",
+                          longitude = "longitude=13.41",
+                          daily = "daily=temperature_2m_min",
+                          start = "start_date=2023-01-27",
+                          end = "end_date=2023-02-26",
+                          timezone = "timezone=GMT-0");
+
+    let response = reqwest::get(&request_url)
+        .await
+        .unwrap()
+        .text()
+        .await;
+    println!("{:?}", response);
+    Ok(())
 }
